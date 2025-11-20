2a7f50c4aea03e63f0376ad0026e183d8ec5fc00
JonasHofreiter <jonas.hofreiter@gmail.com>
2025-11-20 11:17:03 +0100

    Anfang

--- src/JH/WebscraperTest/Scraper.java
+++ src/JH/WebscraperTest/Scraper.java
@@ -3,19 +3,23 @@ package JH.WebscraperTest;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.jsoup.nodes.*;
 import java.io.*;
+import org.jsoup.select.*;
 
 public class Scraper{
 	public static void main (String[] args){
 //		films = ["https://www.synchronkartei.de/film/1467, https://www.synchronkartei.de/film/1469, https://www.synchronkartei.de/film/1468, https://www.synchronkartei.de/film/1421"]
-		Document doc = null;
-		Element spkr = null;
-		String inf = null;
 		try {
-			doc = Jsoup.parse("https://www.synchronkartei.de/film/29457");
-			spkr = doc.select("tbody").first();
-			inf = spkr.body() .text();
+			//Document doc = Jsoup.parse("https://www.synchronkartei.de/film/29457");
+			Document doc = Jsoup.connect("https://www.synchronkartei.de/person/YsnQnNOBZ/sprecher").get();
+//			Document doc = Jsoup.parse("<a>???<a><a href='lalal'>what?<a>");
+			Elements spkr = doc.select("li");
+			System.out.println(spkr.size());
+			for (Element li : spkr){
+				String inf = li.text();
+				System.out.println(inf);
+			}
 		} catch (IOException e){
 			e.printStackTrace();
 		}
