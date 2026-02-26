61b2799d1a8f614d7621951b0578130d8cdd2ee4
Shalxu989 <alexandernass@outlook.de>
2026-02-26 16:48:47 +0100

    Film- & Rollensuche

--- src/AN/Webscraper/Scraper.java
+++ src/AN/Webscraper/Scraper.java
@@ -1,6 +1,7 @@
 package AN.Webscraper;
 
 import java.util.Scanner;
+
 import java.nio.file.*;
 import java.util.Arrays;
 
@@ -12,7 +13,12 @@ import org.jsoup.select.*;
 import com.google.gson.Gson;
 
 public class Scraper {
+  boolean isInstance(Object object, Class<?> type) {
+    return type.isInstance(object);
+  }
+
   public static void main(String[] args) {
+
     Gson gson = new Gson();
     Scanner sc = new Scanner(System.in);
 
@@ -37,29 +43,63 @@ public class Scraper {
     System.out.println("Welchen Film / Welche Serie schaust du gerade?");
     String currentWatch = sc.nextLine();
 
-    System.out.println("Welcher Charakter kommt dir bekannt vor?");
+    System.out.println("Wessen Charakters Stimme kommt dir bekannt vor?");
     String currentWatchCharacter = sc.nextLine();
 
-    System.out.println("Link des Sprechers eingeben:");
-    String speakerLink = sc.nextLine();
+    String currentWatchLink = "";
+
+    for (Film Film : allFilms) {
+      if (currentWatch.equals(Film.title) || currentWatch.equals(Film.origTitle)) {
+        currentWatchLink = Film.url;
+        System.out.println(currentWatchLink);
+      } else if (Film.altTitles != null) {
+        for (String altTitle : Film.altTitles) {
+          if (altTitle.equals(currentWatch)) {
+            currentWatchLink = Film.url;
+            System.out.println(currentWatchLink);
+          }
+        }
+      }
+    }
 
     try {
-      Document doc = Jsoup.connect(speakerLink).get();
-      Elements speaker = doc.select("li");
-      Element speakerNameHTML = doc.select("h1").first();
+      Document filmDoc = Jsoup.connect(currentWatchLink).get();
+      Elements allTDs = filmDoc.select("td");
+      Element formerTD = allTDs.first();
 
-      System.out.println("Durchsuchen von " + speaker.size() + " Sprechrollen von " + speakerNameHTML.text() + "...");
-
-      for (Element li : speaker) {
-        String roleHTML = li.html();
-        for (String film : knownFilms)
-          if (roleHTML.contains(film + "</a>")) {
-            Element role = li.getElementsByTag("em").first();
-            System.out.println("Bekannte Rolle: " + role.text() + " aus " + film);
-          }
+      for (Element td : allTDs) {
+        if (td.text().equals(currentWatchCharacter)) {
+          System.out.println(formerTD);
+        }
+        formerTD = td;
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    /*
+     * System.out.println("Link des Sprechers eingeben:");
+     * String speakerLink = sc.nextLine();
+     * 
+     * try {
+     * Document doc = Jsoup.connect(speakerLink).get();
+     * Elements speaker = doc.select("li");
+     * Element speakerNameHTML = doc.select("h1").first();
+     * 
+     * System.out.println("Durchsuchen von " + speaker.size() + " Sprechrollen von "
+     * + speakerNameHTML.text() + "...");
+     * 
+     * for (Element li : speaker) {
+     * String roleHTML = li.html();
+     * for (String film : knownFilms)
+     * if (roleHTML.contains(film + "</a>")) {
+     * Element role = li.getElementsByTag("em").first();
+     * System.out.println("Bekannte Rolle: " + role.text() + " aus " + film);
+     * }
+     * }
+     * } catch (IOException e) {
+     * e.printStackTrace();
+     * }
+     */
   }
 }
